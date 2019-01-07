6c3ba3d29664f5e2d55e78f784c9d8b28125e3fc
jacky <jacky@localhost.localdomain>
2019-01-07 21:35:09 +0800

    bak

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 	"log"
 	"os"
 	"encoding/json"
+	"strconv"
 )
 
 var client *elastic.Client
-var host = "http://192.168.0.8:9200/"
+var host = "http://127.0.0.1:9200/"
 
 func main() {
 	router := gin.Default()
@@ -24,18 +25,21 @@ func main() {
 	var res *elastic.SearchResult
 	// Query string parameters are parsed using the existing underlying request object.
 	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
-	router.GET("/product-list", func(c *gin.Context) {
-		termsQuery := elastic.NewTermsQuery("category.category_id", "2").Boost(1)
+	router.GET("/products/:categoryId/:page/:size", func(c *gin.Context) {
+		categoryId := c.Param("categoryId")
+		page, _ := strconv.Atoi(c.Param("page"))
+		size, _ := strconv.Atoi(c.Param("size"))
+
+		termsQuery := elastic.NewTermsQuery("category.category_id", categoryId).Boost(1)
 		boolQ := elastic.NewBoolQuery()
 		boolQ = boolQ.Must(termsQuery)
 		nestedQ := elastic.NewNestedQuery("category", boolQ)
 		nBoolQ := elastic.NewBoolQuery()
 		nBoolQ = nBoolQ.Must(nestedQ)
 		constantScoreQ := elastic.NewConstantScoreQuery(nBoolQ)
-		res, err = client.Search().Index("magento2_default_catalog_product").Type("product").Query(constantScoreQ).From(0).Size(20).Do(context.Background())
+		res, err = client.Search().Index("magento2_default_catalog_product").Type("product").Query(constantScoreQ).From(page).Size(size).Do(context.Background())
 		b, _ := json.Marshal(res)
 		c.String(200, string(b))
 	})
 	router.Run(":8080")
 }
-
